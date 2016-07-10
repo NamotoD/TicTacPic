a7b6b788add2cec979688daf061c7f0747e3a633
egonjouda <egonjouda@yahoo.com.au>
2016-07-10 17:02:15 +0000

    make it better

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://egonjouda:Belfast78@ds023634.mlab.com:23634/heroku_tj601g3z' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://egonjouda:Belfast78@ds017165.mlab.com:17165/my_mongo' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
