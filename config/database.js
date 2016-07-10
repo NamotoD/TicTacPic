5fb8795f34ce4cd5a69d6b83346d872e739ff75a
egonjouda <egonjouda@yahoo.com.au>
2016-07-10 18:01:23 +0000

    my first commit

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://egonjouda-advanced_chat-cloned-3444279:27017/myMongo' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://egon:Belfast78@ds017165.mlab.com:17165/my_mongo' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
