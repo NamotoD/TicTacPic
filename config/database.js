ea899f6c5edd0d5802c82d33e2f2f03d496b5aa3
egonjouda <egonjouda@yahoo.com.au>
2016-07-11 16:24:35 +0000

    make it better

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb://egon:Belfast78@ds017165.mlab.com:17165/heroku_n9hnhmqj' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://heroku_n9hnhmqj:7mfhup2uv1lqv5a1h7ffus6qsc@ds017165.mlab.com:17165/heroku_n9hnhmqj' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
