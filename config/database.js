c70e29a1e2a6ef0972c4ec7c2271e120d7f36a4b
egonjouda <egonjouda@yahoo.com.au>
2018-03-18 15:02:26 +0000

    Update .gitignore

--- config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-    'url' : 'mongodb://heroku_n9hnhmqj:7mfhup2uv1lqv5a1h7ffus6qsc@ds017165.mlab.com:17165/heroku_n9hnhmqj' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-};
